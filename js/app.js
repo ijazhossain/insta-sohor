6d2e3c99dababdae312064f5e4f23dfe4e768524
ijaz35 <ijaz.hossain1@gmail.com>
2023-03-12 23:43:05 +0600

    more bug solved

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ const reportPost = (id) => {
 };
 
 const displayContent = (text) => {
-  return text.length < 30 ? 'text' : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
+  return text.length < 30 ? text : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
 };
 
 const switchTab = (id) => {
@@ -64,7 +64,7 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${image}" alt="User Picture" />
+                    <img src="${post.userImage}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">${userName}</a>
                 </div>
@@ -155,7 +155,7 @@ const displayLikedPosts = () => {
 const displayReportedPosts = () => {
   const reportedPosts = getReportedPosts();
   document.getElementById("reported").innerHTML = '';
-  posts.forEach((post) => {
+  reportedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById("reported").appendChild(div);
   });
